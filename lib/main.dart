3ab8ae01f51ee1dc85a135f9009e3a07ea5eac86
lumingrui <lomirus@qq.com>
2021-10-15 21:11:44 +0800

    feat(navigation): bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,36 @@ class MyDrawer extends StatelessWidget {
   }
 }
 
+class Navigation extends StatefulWidget {
+  NavigationState createState() => NavigationState();
+}
+
+class NavigationState extends State<Navigation> {
+  int _selectedIndex = 0;
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.category),
+          label: "Category",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.leaderboard),
+          label: "Ranking",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite),
+          label: "Favorite",
+        ),
+      ],
+      onTap: (int index) => setState(() {
+        _selectedIndex = index;
+      }),
+      currentIndex: _selectedIndex,
+    );
+  }
+}
+
 class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -37,6 +67,7 @@ class App extends StatelessWidget {
         appBar: AppBar(title: Text("Hall of Fame")),
         drawer: MyDrawer(),
         body: Center(child: Text("Hello World")),
+        bottomNavigationBar: Navigation(),
       ),
     );
   }
