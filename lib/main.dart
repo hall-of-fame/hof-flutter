125e4b668127cb997394bc7407287b507bdfa3fd
lumingrui <lomirus@qq.com>
2021-10-15 20:15:52 +0800

    feat(drawer): basic drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,33 @@
 import 'package:flutter/material.dart';
 
+class MyDrawer extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(color: Colors.green),
+            child: Text(
+              "Hall of Fame",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text("Settings"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: Icon(Icons.info),
+            title: Text("About"),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -7,6 +35,7 @@ class App extends StatelessWidget {
       theme: ThemeData(primaryColor: Colors.green),
       home: Scaffold(
         appBar: AppBar(title: Text("Hall of Fame")),
+        drawer: MyDrawer(),
         body: Center(child: Text("Hello World")),
       ),
     );
