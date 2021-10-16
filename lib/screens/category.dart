59e600c846e1097241077d727420eed718fde365
lumingrui <lomirus@qq.com>
2021-10-16 20:59:40 +0800

    feat(category): network request

--- lib/screens/category.dart
+++ lib/screens/category.dart
@@ -1,7 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
 
-class CategoryScreen extends StatelessWidget {
+class CategoryScreen extends StatefulWidget {
+  _CategoryScreenState createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  String res = "";
+  // 0 loading, 1 success, 2 failed
+  int status = 0;
+
+  initState() {
+    super.initState();
+    fetchData();
+  }
+
+  void fetchData() async {
+    final url = Uri.parse('https://zhongtai521.wang:996/departments/');
+    final prefs = await SharedPreferences.getInstance();
+    final response = await http.get(url, headers: {
+      "Authorization": prefs.getString("password") ?? "",
+    });
+
+    if (response.statusCode == 200) {
+      setState(() {
+        res = response.body;
+        status = 1;
+      });
+    } else {
+      setState(() {
+        status = 2;
+      });
+    }
+  }
+
   Widget build(BuildContext context) {
-    return Center(child: Text("Category"));
+    return ListView(
+      children: [
+        Text(status.toString()),
+        Text(res),
+      ],
+    );
   }
 }
