b0e67c0052e05f84a5a95d57d2ad35e5f450ae42
lumingrui <lomirus@qq.com>
2021-10-16 17:18:04 +0800

    feat(settings): save the password with shared preferences

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  String password = "";
+
+  SimpleDialog _passwordDialog(BuildContext context) {
+    return SimpleDialog(
+      children: [
+        Container(
+          padding: EdgeInsets.fromLTRB(24, 8, 24, 0),
+          child: TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: "New Password",
+            ),
+            onChanged: (value) => setState(() {
+              password = value;
+            }),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(24, 16, 24, 0),
+          child: Row(
+            textDirection: TextDirection.rtl,
+            children: [
+              TextButton(
+                onPressed: () async {
+                  final prefs = await SharedPreferences.getInstance();
+                  prefs.setString("password", password);
+                  Navigator.pop(context, "Ok");
+                },
+                child: Text("Ok"),
+              ),
+              TextButton(
+                onPressed: () => Navigator.pop(context, "Cancel"),
+                child: Text("Cancel"),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Settings")),
-      body: Center(child: Text("Settings")),
+      body: Container(
+        padding: EdgeInsets.all(24.0),
+        child: ListTile(
+          title: Text("Password"),
+          leading: Icon(Icons.password),
+          trailing: Icon(Icons.edit),
+          onTap: () => showDialog(
+            context: context,
+            builder: _passwordDialog,
+          ),
+        ),
+      ),
     );
   }
 }
