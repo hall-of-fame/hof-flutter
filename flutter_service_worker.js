6c60dc09856b5d23827b1355fa162d7de697ed2e
lomirus <lomirus@qq.com>
2021-10-26 06:50:07 +0000

    Deploying to gh-pages from @ hall-of-fame/hof-flutter-md@32a14961ff6ea7ce3ff344e327f24d3e766a4c53 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,8 +13,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "main.dart.js": "d5d48e0d5af8782790c677d0c3cac4de",
 "manifest.json": "ff18b5c7132cd9d928975d1b7867b02e",
-"index.html": "70293ddc2f411d851ae2e360fdcbb6f6",
-"/": "70293ddc2f411d851ae2e360fdcbb6f6"
+"index.html": "6a4c53c13502a4fa4b9c0a4bcce03e00",
+"/": "6a4c53c13502a4fa4b9c0a4bcce03e00"
 };
 
 // The application shell files that are downloaded before a service worker can
