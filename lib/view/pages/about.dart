e6a4f36d5d4b1438149ff1cf44396ac2c9c36449
lomirus <lomirus@qq.com>
2022-04-09 15:52:20 +0800

    feat: enrich the about page

--- lib/view/pages/about.dart
+++ lib/view/pages/about.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+const String version = '0.1.0';
 
 class AboutScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("About")),
-      body: Center(child: Text("About")),
+      body: Column(
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(24),
+                  child: Image.asset(
+                    "assets/logo.png",
+                    width: 120,
+                  ),
+                ),
+                Container(margin: EdgeInsets.all(8.0)),
+                Text(
+                  "Hall of Fame",
+                  style: TextStyle(fontSize: 24),
+                ),
+                Container(margin: EdgeInsets.all(2.0)),
+                Text(
+                  "Version: $version",
+                  style: TextStyle(color: Colors.grey),
+                ),
+                Container(margin: EdgeInsets.all(8.0)),
+                Text(
+                  "Made with Flutter, by Redrock",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(24, 0, 24, 8),
+            child: ListTile(
+              leading: Icon(Icons.link),
+              title: Text("Github Repository"),
+              onTap: () =>
+                  launch("https://github.com/hall-of-fame/hof-flutter-md"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
