088b821f1c9fd30be5bf0599ac4a7a90c2b62d85
mirus <lomirus.dev@protonmail.com>
2025-10-28 22:01:56 +0800

    chore: update android configurations

--- android/app/src/main/kotlin/com/example/flutter_application/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.hall_of_fame
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/example/hall_of_fame/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.redrock.hall_of_fame
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
